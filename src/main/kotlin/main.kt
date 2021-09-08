239589b572ef4b2b46c841bbb355cdf1fba7982b
buyolitsez <hamerfall2003@yandex.ru>
2021-09-08 22:26:44 +0300

    Reviewed code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,33 +2,45 @@
 //Implementation of LCS(https://en.wikipedia.org/wiki/Longest_common_subsequence_problem)
 // Takes two arrays of strings and returns their longest common subsequence
 // LCS({"A", "B", "C", "D"}, {"D", "C", "D", "A"}) = {"C", "D"}
-fun LCS(text1: Array<String>, text2: Array<String>): Array<String> {
+
+
+fun lcs(text1: Array<String>, text2: Array<String>): Array<String> {
     val n = text1.size
     val m = text2.size
-    val longestCommonSubsequnceOnPrefixes = Array(n + 1, {Array<Int>(m + 1, {0})})
-    val directionOnMatrix = Array(n + 1, {Array<Int>(m + 1, {0})}) //needs to return the answer array of strings (0 for left + up, -1 for left, 1 for up)
+    val lcsOnPrefixes = Array(n + 1) { Array(m + 1) { 0 } }
+    val directionOnMatrix = Array(n + 1){Array(m + 1) { 0 }} //needs to return the answer array of strings (0 for left + up, -1 for left, 1 for up)
     for (i in 1..n) {
         for (j in 1..m) {
             if (text1[i - 1] == text2[j - 1]) {
-                longestCommonSubsequnceOnPrefixes[i][j] = longestCommonSubsequnceOnPrefixes[i - 1][j - 1] + 1
+                lcsOnPrefixes[i][j] = lcsOnPrefixes[i - 1][j - 1] + 1
                 directionOnMatrix[i][j] = 0
             } else {
-                if (longestCommonSubsequnceOnPrefixes[i - 1][j] >= longestCommonSubsequnceOnPrefixes[i][j - 1]) {
-                    longestCommonSubsequnceOnPrefixes[i][j] = longestCommonSubsequnceOnPrefixes[i - 1][j]
+                if (lcsOnPrefixes[i - 1][j] >= lcsOnPrefixes[i][j - 1]) {
+                    lcsOnPrefixes[i][j] = lcsOnPrefixes[i - 1][j]
                     directionOnMatrix[i][j] = -1
                 } else {
-                    longestCommonSubsequnceOnPrefixes[i][j] = longestCommonSubsequnceOnPrefixes[i][j - 1]
+                    lcsOnPrefixes[i][j] = lcsOnPrefixes[i][j - 1]
                     directionOnMatrix[i][j] = 1
                 }
             }
         }
     }
-    var answerLen = longestCommonSubsequnceOnPrefixes[n][m]
+    return calculateResultLCS(lcsOnPrefixes, n, m, directionOnMatrix, text1)
+}
+
+private fun calculateResultLCS(
+    longestCommonSubsequenceOnPrefixes: Array<Array<Int>>,
+    n: Int,
+    m: Int,
+    directionOnMatrix: Array<Array<Int>>,
+    text1: Array<String>
+): Array<String> {
+    var answerLen = longestCommonSubsequenceOnPrefixes[n][m]
     //calculating the result array
-    val answerArray = Array(answerLen, {""})
+    val answerArray = Array(answerLen) { "" }
     var lenText1 = n
     var lenText2 = m
-    while(answerLen > 0) {
+    while (answerLen > 0) {
         if (directionOnMatrix[lenText1][lenText2] == 0) {
             answerLen--
             --lenText1
--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -10,32 +10,32 @@ internal class Test1 {
 internal class TestLCS {
     @Test
     fun testGroup1() {
-        assertContentEquals(arrayOf("a", "b"), LCS(arrayOf("a", "b"), arrayOf("a", "b")))
-        assertContentEquals(arrayOf("b", "a"), LCS(arrayOf("a", "b", "a"), arrayOf("b", "a")))
-        assertContentEquals(arrayOf("a"), LCS(arrayOf("a", "b"), arrayOf("ba", "a")))
-        assertContentEquals(arrayOf(), LCS(arrayOf("aqwe", "bzx"), arrayOf("abasd", "bdqwd")))
+        assertContentEquals(arrayOf("a", "b"), lcs(arrayOf("a", "b"), arrayOf("a", "b")))
+        assertContentEquals(arrayOf("b", "a"), lcs(arrayOf("a", "b", "a"), arrayOf("b", "a")))
+        assertContentEquals(arrayOf("a"), lcs(arrayOf("a", "b"), arrayOf("ba", "a")))
+        assertContentEquals(arrayOf(), lcs(arrayOf("aqwe", "bzx"), arrayOf("abasd", "bdqwd")))
     }
 
     @Test
     fun testGroup2() {
-        assertContentEquals(arrayOf("a", "c", "a"), LCS(arrayOf("a", "b", "c", "a"), arrayOf("a", "c", "e", "a")))
-        assertContentEquals(arrayOf("e"), LCS(arrayOf("b", "e"), arrayOf("f", "f", "g", "e")))
-        assertContentEquals(arrayOf("aqwe", "sadasd"), LCS(arrayOf("aqwe", "bzx", "sadasd", "bzx"), arrayOf("bzx", "bzy", "aqwe", "bdqwd", "sadasd")))
-        assertContentEquals(arrayOf("0", "2", "56"), LCS(arrayOf("0", "1", "2", "3", "4", "56"), arrayOf("0", "2", "56", "1", "1", "5", "7")))
+        assertContentEquals(arrayOf("a", "c", "a"), lcs(arrayOf("a", "b", "c", "a"), arrayOf("a", "c", "e", "a")))
+        assertContentEquals(arrayOf("e"), lcs(arrayOf("b", "e"), arrayOf("f", "f", "g", "e")))
+        assertContentEquals(arrayOf("aqwe", "sadasd"), lcs(arrayOf("aqwe", "bzx", "sadasd", "bzx"), arrayOf("bzx", "bzy", "aqwe", "bdqwd", "sadasd")))
+        assertContentEquals(arrayOf("0", "2", "56"), lcs(arrayOf("0", "1", "2", "3", "4", "56"), arrayOf("0", "2", "56", "1", "1", "5", "7")))
     }
 
     @Test
     fun testGroup3() {
-        assertContentEquals(arrayOf("X", "M", "J", "A", "Z"), LCS(arrayOf("X", "M", "J", "Y", "A", "U", "Z"), arrayOf("X", "M", "J", "A", "A", "T", "Z")))
-        assertContentEquals(arrayOf(), LCS(arrayOf("x", "m", "J", "Y", "A", "U", "Z"), arrayOf("X", "M", "j", "a", "a", "t", "z")))
-        assertContentEquals(arrayOf("-1"), LCS(arrayOf("1", "-1"), arrayOf("-1", "0")))
-        assertContentEquals(arrayOf("ANcb-%3q23"), LCS(arrayOf("ANcb-%3q23"), arrayOf("ANcb-%3q23")))
+        assertContentEquals(arrayOf("X", "M", "J", "A", "Z"), lcs(arrayOf("X", "M", "J", "Y", "A", "U", "Z"), arrayOf("X", "M", "J", "A", "A", "T", "Z")))
+        assertContentEquals(arrayOf(), lcs(arrayOf("x", "m", "J", "Y", "A", "U", "Z"), arrayOf("X", "M", "j", "a", "a", "t", "z")))
+        assertContentEquals(arrayOf("-1"), lcs(arrayOf("1", "-1"), arrayOf("-1", "0")))
+        assertContentEquals(arrayOf("ANcb-%3q23"), lcs(arrayOf("ANcb-%3q23"), arrayOf("ANcb-%3q23")))
     }
 
     @Test
     fun testGroup4() {
-        assertContentEquals(arrayOf(), LCS(arrayOf(), arrayOf("abc", "asd")))
-        assertContentEquals(arrayOf(), LCS(arrayOf("1", "2"), arrayOf()))
-        assertContentEquals(arrayOf(), LCS(arrayOf(), arrayOf()))
+        assertContentEquals(arrayOf(), lcs(arrayOf(), arrayOf("abc", "asd")))
+        assertContentEquals(arrayOf(), lcs(arrayOf("1", "2"), arrayOf()))
+        assertContentEquals(arrayOf(), lcs(arrayOf(), arrayOf()))
     }
 }
