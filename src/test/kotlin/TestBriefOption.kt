d505fc77f56c6c1ecea2b2d417952e25bdd33ddf
buyolitsez <hamerfall2003@yandex.ru>
2021-09-16 15:52:09 +0300

    Add tests for --brief option

---
+++ src/test/kotlin/TestBriefOption.kt
@@ -0,0 +1,49 @@
+import java.io.ByteArrayOutputStream
+import kotlin.test.*
+import java.io.PrintStream
+import kotlin.test.BeforeTest
+import kotlin.test.AfterTest
+
+internal class TestBriefOption {
+    private val standardOut = System.out
+    private val standardIn = System.`in`
+    private val stream = ByteArrayOutputStream()
+
+    @BeforeTest
+    fun BeforeTest() = run { OPTIONS["brief"] = true }
+
+    @BeforeTest
+    fun setUp() {
+        System.setOut(PrintStream(stream))
+    }
+
+    @AfterTest
+    fun tearDown() {
+        System.setOut(standardOut)
+        System.setIn(standardIn)
+    }
+
+    @Test
+    fun testAbsoluteDifferent() {
+        printDiffResult(arrayOf("a", "b"), arrayOf("c, d"))
+        assertEquals("Files src/files/text1.txt and src/files/text2.txt differ", stream.toString().trim())
+    }
+
+    @Test
+    fun testDifferent() {
+        printDiffResult(arrayOf("a", "b", "c"), arrayOf("a", "c", "b"))
+        assertEquals("Files src/files/text1.txt and src/files/text2.txt differ", stream.toString().trim())
+    }
+
+    @Test
+    fun testEmptyInput() {
+        printDiffResult(arrayOf(), arrayOf())
+        assertEquals("", stream.toString().trim())
+    }
+
+    @Test
+    fun testEqualInput() {
+        printDiffResult(arrayOf("a", "b", "c"), arrayOf("a", "b", "c"))
+        assertEquals("", stream.toString().trim())
+    }
+}
